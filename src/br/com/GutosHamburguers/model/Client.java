38945c1419f240a9f3147344a14b60fa6cea9a84
guto16 <gangueextreme@gmail.com>
2022-06-17 13:32:16 -0300

    packages e models

---
+++ src/br/com/GutosHamburguers/model/Client.java
@@ -0,0 +1,36 @@
+
+package br.com.GutosHamburguers.model;
+
+
+public class Client {
+    private String nameClient;
+    private String address;
+    private String phone;
+
+    public String getNameClient() {
+        return nameClient;
+    }
+
+    public void setNameClient(String nameClient) {
+        this.nameClient = nameClient;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    
+
+}
